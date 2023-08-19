e359d38b4084344a89af856c2534b229f7aef5ca
vishionx <vishakhadikshit3@gmail.com>
2023-08-19 18:54:19 -0400

    removed_comment

--- face_recognition.py
+++ face_recognition.py
@@ -10,15 +10,9 @@ cv2.rectangle(img,
               (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
               (0,155,255),
               2)
-# cv2.circle(img,(keypoints['left_eye']), 2, (0,155,255), 2)
-# cv2.circle(img,(keypoints['right_eye']), 2, (0,155,255), 2)
-# cv2.circle(img,(keypoints['nose']), 2, (0,155,255), 2)
-# cv2.circle(img,(keypoints['mouth_left']), 2, (0,155,255), 2)
-# cv2.circle(img,(keypoints['mouth_right']), 2, (0,155,255), 2)
 cv2.imwrite("face_drawn2.jpg", img)
 cv2.namedWindow("img")
 cv2.imshow("img",img)
 cv2.waitKey(0)
-#(x, y, w, h) = cv2.boundingRect(img)
 cr_img = img[bounding_box[1]: bounding_box[1]+bounding_box[3], bounding_box[0]: bounding_box[0]+bounding_box[2]]
 cv2.imwrite("face_crop2.jpg", cr_img)
